12b2fb0f5c46ca7438c9057e8c52d20a36d00d3f
wuhanstudio <wuhanstudio@qq.com>
2022-03-17 08:47:11 +0000

    Add nodejs server

---
+++ server/resources/js/script.js
@@ -0,0 +1,52 @@
+var socket = io();
+
+// Capture image
+function capture() {
+    socket.emit('capture');
+}
+
+// Start recording
+function start() {
+    socket.emit('start');
+    $('#start').prop('disabled', true);
+    $('#stop').prop('disabled', false);
+}
+
+// Stop recording
+function stop() {
+    socket.emit('stop');
+    $('#start').prop('disabled', false);
+    $('#stop').prop('disabled', true);
+}
+
+// Receive captured image
+socket.on('capture', (img) => {
+    $('#capture').attr("src", img);
+});
+
+// Receive Encoded Video
+socket.on('encoded', (img) => {
+    // setTimeout(function () {
+    //     location.reload();
+    // }, 5000);
+    Snackbar.show({
+        text: 'Please restart the browser to see the video',
+        pos: 'bottom-right',
+        duration: 5000
+    });    
+});
+
+// Log message
+socket.on('info', (msg) => {
+    Snackbar.show({
+        text: msg,
+        pos: 'bottom-right',
+        duration: 5000
+    });
+});
+
+// User connected
+socket.on('connect', () => {
+    console.log('Session Id: ', socket.id);
+});
+
